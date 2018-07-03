d29ce02a15baa9a052ae7e00bf300ab08bb4750a
Stefany Go <stefany_go@hotmail.com>
2018-07-03 14:41:25 -0400

    Added Styles Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react'
+
 import SideBar from './SideBar'
 import Chat from './Chat'
 
 class Main extends Component {
     render() {
         return(
-            <div className="Main">
+            <div className="Main" style={styles}>
                 <SideBar />
                 <Chat />
             </div>
         )
     }
+}
+
+const styles = {
+    display: 'flex', 
+    alignItems: 'stretch',
+    height: '100vh',
 }
 
 export default Main
