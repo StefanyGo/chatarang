00f3a8ef1193e28ffa3439ba59195b57a8425b47
Stefany Go <stefany_go@hotmail.com>
2018-07-05 16:07:14 -0400

    Add Sample Firebase Config

--- src/base.example.js
+++ src/base.example.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyCNKO8vNqI7CTq_lHgx05pqisOCUQhLu7c",
-    authDomain: "chatarang-5ea2a.firebaseapp.com",
-    databaseURL: "https://chatarang-5ea2a.firebaseio.com",
-    projectId: "chatarang-5ea2a",
-    storageBucket: "chatarang-5ea2a.appspot.com",
-    messagingSenderId: "1027337084122"
-  };
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  databaseURL: "YOUR DATABASE URL",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "YOUR STORAGE BUCKET",
+  messagingSenderId: "YOUR MESSAGING SENDER ID"
+}
 
-  const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config)
