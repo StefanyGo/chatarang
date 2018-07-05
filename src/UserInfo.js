9dee56bea233116ac7a9ab6e7510176cbd39c329
Stefany Go <stefany_go@hotmail.com>
2018-07-05 14:13:49 -0400

    Add Components- Avatar, SignOutButton, UserInfo

---
+++ src/UserInfo.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Avatar from './Avatar'
+import SignOutButton from './SignOutButton'
+
+const UserInfo = ({user}) => {
+  return (
+    <div
+      className="UserInfo"
+      style={styles.userInfo}
+    >
+      <Avatar user={user} />
+      <div style={styles.user}>
+        {user.displayName}
+      </div>
+      <SignOutButton />
+    </div>
+  )
+}
+
+const styles = {
+  userInfo: {
+    padding: '0 1rem',
+    marginBottom: '1rem',
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  user: {
+    flex: 1,
+  }
+}
+
+export default UserInfo
